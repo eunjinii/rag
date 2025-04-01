1944736c5dcce6fd40642da2923f192b6f21f021
eunjinii <iam.eunjinii@gmail.com>
2025-04-01 18:05:37 +0900

    Refactor knowledge RAG architecture

--- question_answering.py
+++ question_answering.py
@@ -22,17 +22,25 @@ class QuestionAnswering:
     
     def load_test_dataset(self):
         print("Loading dataset...")
-        if self.dataset_type in ["MedMCQA", "MedQuad"]:
+        if self.dataset_type == "MedQuad":
             dataset = load_dataset(self.dataset_name, split="train")
-            split_dataset = dataset.train_test_split(test_size=0.1)
+            split_dataset = dataset.train_test_split(test_size=0.01) # FIXME: test_size=0.1
             test_dataset = split_dataset["test"]
+            return test_dataset
+        
+        elif self.dataset_type == "MedMCQA":
+            dataset = load_dataset(self.dataset_name, split="train")
+            split_dataset = dataset.train_test_split(test_size=0.01) # FIXME: test_size=0.1
+            test_dataset = split_dataset["test"]
+            valid_subjects = ['Anatomy', 'Social & Preventive Medicine', 'Pathology', 'Medicine', 'Pharmacology', 'Gynaecology & Obstetrics', 'Physiology', 'Microbiology', 'Biochemistry', 'Pediatrics']
+            test_dataset = test_dataset.filter(lambda row: row["subject_name"] in valid_subjects)
             return test_dataset
 
         elif self.dataset_type == "MMLU":
-            subject_list = ["anatomy", "clinical_knowledge", "college_biology", "college_medicine", "human_aging", "medical_genetics", "nutrition", "professional_medicine"]
+            subject_list = ["anatomy", "clinical_knowledge", "college_biology", "college_medicine", "high_school_biology", "human_aging", "medical_genetics", "nutrition", "professional_medicine", "virology"]
             all_datasets = []
             for subject in subject_list:
-                dataset = load_dataset("NLPCoreTeam/mmlu_ru", name=subject, split="test")
+                dataset = load_dataset(self.dataset_name, name=subject, split="test")
                 dataset = dataset.map(lambda example: {**example, "subject": subject})
                 all_datasets.append(dataset)
             test_dataset = concatenate_datasets(all_datasets)
@@ -59,6 +67,7 @@ class QuestionAnswering:
             df['answer'] = df['answerindex'].map(ANSWER_MAPPING_DICT)
             df['options'] = df.apply(lambda row: [row['opa'], row['opb'], row['opc'], row['opd']], axis=1)
             df.drop(columns=["id", "opa", "opb", "opc", "opd", "exp", "topic_name", "choice_type", "answerindex"], inplace=True)
+            df = df[df['subject'] != 'Unknown']
             
         elif self.dataset_type == "MedQA":
             df.rename(columns={"sent1": "question"}, inplace=True)
@@ -67,9 +76,8 @@ class QuestionAnswering:
             df.drop(columns=["id", "sent2", "ending0", "ending1", "ending2", "ending3", "label"], inplace=True)
 
         elif self.dataset_type == "MMLU":
-            df.rename(columns={"question_en": "question", "choices_en":"options"}, inplace=True)
+            df.rename(columns={"choices":"options"}, inplace=True)
             df['answer'] = df['answer'].map(ANSWER_MAPPING_DICT)
-            df.drop(columns=["question_ru", "choices_ru"], inplace=True)
         
         # elif self.dataset_type == "MedQuad":
         #     df.rename(columns={"Question": "question", "Answer":"answer"}, inplace=True)
